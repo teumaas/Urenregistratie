aafe617a45f7bf8b145cc2138c38d6b8f2e863e0
jordibastiaansen <jordi.bastiaansen@gmail.com>
2017-05-18 09:47:50 +0200

    added models/ controllers
    controllers toegevoegd

--- CCO_Urenregistratie/CCO_Urenregistratie/Models/ProjectModels.cs
+++ CCO_Urenregistratie/CCO_Urenregistratie/Models/ProjectModels.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 
 namespace CCO_Urenregistratie.Models
@@ -15,11 +18,12 @@ namespace CCO_Urenregistratie.Models
     {
         [Key]
         public int Id { get; set; }
-        public int UserId { get; set; }
-
+        public string UserId { get; private set; }
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public string Color { get; set; }
         public virtual ICollection<Tasks> Tasks { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
     }
 }
--- CCO_Urenregistratie/CCO_Urenregistratie/Models/TaskModels.cs
+++ CCO_Urenregistratie/CCO_Urenregistratie/Models/TaskModels.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 
 namespace CCO_Urenregistratie.Models
@@ -15,7 +18,7 @@ namespace CCO_Urenregistratie.Models
     {
         [Key]
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; private set; }
         public int ProjectId { get; set; }
 
         [DataType(DataType.MultilineText)]
@@ -23,5 +26,7 @@ namespace CCO_Urenregistratie.Models
         public DateTime Startdate { get; set; }
         public DateTime Enddate { get; set; }
         public virtual Project Project { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
     }
 }
